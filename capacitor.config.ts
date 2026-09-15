70733e8ae4acf64c1e0a697bca3d9c1651eea3c7
Y2003Er <yusuphhanigomba7@gmail.com>
2026-09-15 17:24:48 +0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tech26.bot',
+  appName: '26 Tech Bot',
+  webDir: 'dist'
+};
+
+export default config;
